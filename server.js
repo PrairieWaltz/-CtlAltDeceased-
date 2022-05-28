8ace22aba0be031c6dc036b26c1afcb5e57531f2
PrairieWaltz <85768337+PrairieWaltz@users.noreply.github.com>
2022-05-28 15:21:10 -0400

    Rebuild

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 const app = express();
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
+
+
+
+mongoose.connect("mongodb://localhost:27017/ctlaltdelete", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Mongoose is a GO");
+});
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
 
 app.use("/articles", articleRouter);
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article",
-      createdAt: new Date(),
-      description: "Test Description",
-    },
-    {
-      title: "Test Article 2",
-      createdAt: new Date(),
-      description: "Test Description 2",
-    },
-  ];
-  res.render("articles/index", { articles: articles });
-}); 
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Loud & Clear on Port 3000");
+});
