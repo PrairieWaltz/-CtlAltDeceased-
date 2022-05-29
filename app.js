60fca153c067b1a280468726d029e92b5b9096ab
PrairieWaltz <85768337+PrairieWaltz@users.noreply.github.com>
2022-05-29 17:45:01 -0400

    Rebuild node.js

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
-const ejs = require("ejs");
 const express = require("express");
+const path = require("path");
+const ejsMate = require("ejs-mate");
 
-const date = new Date().toLocaleDateString();
+const port = 3000;
+
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+// WEB SOCKET
+
+app.rawListeners("echo", (ws, req) => {
+  ws.on("message", (msg) => {
+    ws.send(msg);
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Loud and Clear on Port ${port}`);
+});
